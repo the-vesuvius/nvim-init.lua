a1b2d404034b6d7d3cb1be8a4112480f38481e1d
the-vesuvius <steven.stas92@gmail.com>
2023-06-26 10:19:49 +0300

    clean up

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,14 +15,12 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<Enter>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
--- lua/vesuvius/packer.lua
+++ lua/vesuvius/packer.lua
@@ -47,5 +47,9 @@ return require('packer').startup(function(use)
 	  {'L3MON4D3/LuaSnip'},     -- Required
   }
 }
+
 use("github/copilot.vim")
+
+use("jose-elias-alvarez/null-ls.nvim")
+
 end)
